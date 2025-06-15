3dfd8fabdd809a9468e8b67049382c9855565370
rameshbandgar <ramesh.bandgar@gmail.com>
2025-06-15 18:15:58 +0530

    added skills in add and edit employee api and html

--- tutorial/app/models.py
+++ tutorial/app/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+
 class Employee(models.Model):
-    emp_id=models.AutoField(primary_key=True)
-    emp_first_name=models.CharField(max_length=100, null=True,blank=True)
-    emp_last_name=models.CharField(max_length=100, null=True,blank=True)
-    emp_designation=models.CharField(max_length=100, null=True,blank=True)
+    emp_id = models.AutoField(primary_key=True)
+    emp_first_name = models.CharField(max_length=100, null=True, blank=True)
+    emp_last_name = models.CharField(max_length=100, null=True, blank=True)
+    emp_designation = models.CharField(max_length=100, null=True, blank=True)
+    skills = models.CharField(max_length=100, null=True, blank=True)
 
     # class Meta:
     #     db_table='app_employee'
@@ -15,14 +17,14 @@ class Employee(models.Model):
     def __str__(self) -> str:
         # return super().__str__()
         return self.emp_first_name
-    
+
 
 class EmployeeDirectory(models.Model):
-    doc_id=models.AutoField(primary_key=True)
-    directory_name=models.CharField(max_length=100)
-    document_directory=models.CharField(max_length=200)
-    updated_by=models.CharField(max_length=100)
-    updated_at=models.DateTimeField(auto_now=True)
+    doc_id = models.AutoField(primary_key=True)
+    directory_name = models.CharField(max_length=100)
+    document_directory = models.CharField(max_length=200)
+    updated_by = models.CharField(max_length=100)
+    updated_at = models.DateTimeField(auto_now=True)
 
     # class Meta:
     #     managed=True
@@ -30,4 +32,7 @@ class EmployeeDirectory(models.Model):
 
 
 class EmployeeSkills(models.Model):
-    skill_name=models.CharField(max_length=200)
+    skill_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.skill_name
--- tutorial/app/views.py
+++ tutorial/app/views.py
@@ -3,77 +3,105 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
-from .models import Employee
+from .models import Employee, EmployeeSkills
 
-baseUrl="http://127.0.0.1:8000"
+baseUrl = "http://127.0.0.1:8000"
+
 
 # Create your views here.
 def home(request):
     # return HttpResponse("Python is easy")
-    return render (request, 'app/base.html')
+    return render(request, "app/base.html")
+
 
 @csrf_exempt
 def add_employee_data(request):
-    if request.method=="POST":
-        response={}
+    if request.method == "POST":
+        response = {}
         print("in post method")
-        print(f'requets data=====>{request.POST}')
-        emp_first_name=request.POST.get('emp_first_name')
-        emp_last_name=request.POST.get('emp_last_name')
-        emp_designation=request.POST.get('emp_designation')
+        print(f"requets data=====>{request.POST}")
+        emp_first_name = request.POST.get("emp_first_name")
+        emp_last_name = request.POST.get("emp_last_name")
+        emp_designation = request.POST.get("emp_designation")
+        skills = request.POST.getlist('skills[]')
+        print(f"{skills=},{type(skills)=}")
+        skills_str = ','.join(skills)
 
-
-        Employee.objects.create(emp_first_name=emp_first_name,emp_last_name=emp_last_name,
-                                emp_designation=emp_designation).save()
-        response={
-            'status':True,
-            'status_code':200,
-            'message':'Employee Details Added Successfully'
+        Employee.objects.create(
+            emp_first_name=emp_first_name,
+            emp_last_name=emp_last_name,
+            emp_designation=emp_designation,
+            skills=skills_str
+        ).save()
+        response = {
+            "status": True,
+            "status_code": 200,
+            "message": "Employee Details Added Successfully",
         }
         return JsonResponse(response, safe=False)
     else:
-        print('pass the context required in front end')
-        return render(request,'app/addEmployee.html',{'baseUrl':baseUrl})
+        print("pass the context required in front end")
+        skills = EmployeeSkills.objects.all().order_by("id")
+        return render(
+            request, "app/addEmployee.html", {"baseUrl": baseUrl, "skills": skills}
+        )
 
 
 @csrf_exempt
 def edit_employee_data(request, id):
     if request.method == "POST":
         response = {}
-        emp_first_name = request.POST.get('emp_first_name')
-        emp_last_name = request.POST.get('emp_last_name')
-        emp_designation = request.POST.get('emp_designation')
+        emp_first_name = request.POST.get("emp_first_name")
+        emp_last_name = request.POST.get("emp_last_name")
+        emp_designation = request.POST.get("emp_designation")
+        skills = request.POST.getlist('skills[]')
+        print(f"{skills=},{type(skills)=}")
+        skills_str = ','.join(skills)
+        
 
         employee_object = Employee.objects.filter(emp_id=id).update(
             emp_first_name=emp_first_name,
             emp_last_name=emp_last_name,
-            emp_designation=emp_designation
+            emp_designation=emp_designation,
+            skills=skills_str
         )
 
         response = {
-            'status': True,
-            'status_code': 200,
-            'message': 'Employee Details Updated Successfully'
+            "status": True,
+            "status_code": 200,
+            "message": "Employee Details Updated Successfully",
         }
         return JsonResponse(response, safe=False)
     else:
+        skillarray=[]
         employee = Employee.objects.get(emp_id=id)  # Fetch employee details
+        skills=employee.skills
+        print(f"skills===>{skills}")
+        if skills:
+            skillarray=skills.split(',')
+            print(f"skillarray===>{skillarray}")
+        all_skills = list(EmployeeSkills.objects.all().order_by("id"))
+
         # baseUrl = 'your_base_url'  # Define your base URL
-        return render(request, 'app/editEmployee.html', {'baseUrl': baseUrl, 'emp_id': id, 'employee': employee})
+        return render(
+            request,
+            "app/editEmployee.html",
+            {"baseUrl": baseUrl, "emp_id": id, "employee": employee,"skills":skillarray,
+             "all_skills":all_skills}
+        )
 
 
 @csrf_exempt
-def delete_employee_data(request,id):
+def delete_employee_data(request, id):
     Employee.objects.filter(emp_id=id).delete()
     # return render(request, 'app/employee.html',{'baseUrl':baseUrl})
-    return redirect('/showdata')
-
+    return redirect("/showdata")
 
 
 # @csrf_exempt
 # def edit_employee_data(request,id):
 #     # employee_object = Employee.objects.filter(emp_id=id)
-    
+
 #     if request.method=="POST":
 #         response={}
 #         print("in post method")
@@ -86,7 +114,7 @@ def delete_employee_data(request,id):
 #                                 emp_designation=emp_designation)
 
 #         print(f'employee_obj=========>{employee_object}')
-        
+
 #         response={
 #             'status':True,
 #             'status_code':200,
@@ -103,18 +131,18 @@ def show_data(request):
     if request.method == "POST":
         response = {}
         try:
-            body_unicode = request.body.decode('utf-8')
+            body_unicode = request.body.decode("utf-8")
             parsed_data = json.loads(body_unicode)
-            print(f'parsed_data======>{parsed_data}')
+            print(f"parsed_data======>{parsed_data}")
 
-            payload = parsed_data.get('payload')
+            payload = parsed_data.get("payload")
 
             temp = json.loads(payload)
-            
-            print(f'temp========>{temp}')
 
-            emp_id = temp.get('emp_id')
-            
+            print(f"temp========>{temp}")
+
+            emp_id = temp.get("emp_id")
+
             print(f"{emp_id=}, {parsed_data=}")
 
             # payload = request.POST.get('payload')
@@ -125,22 +153,17 @@ def show_data(request):
             # print(f'{parsed_data=},{emp_id=}')
 
             emp_object = list(Employee.objects.filter(emp_id=emp_id).values())
-            print(f'{emp_object=}')
+            print(f"{emp_object=}")
 
-            response = {
-                'status': True,
-                'message': "success",
-                "data": emp_object
-            }
+            response = {"status": True, "message": "success", "data": emp_object}
         except Exception as e:
-            print(f'Error: {e}')
-            response = {
-                'status': False,
-                'message': str(e)
-            }
+            print(f"Error: {e}")
+            response = {"status": False, "message": str(e)}
 
         return JsonResponse(response, safe=False)
     else:
         employee_object = Employee.objects.all().values()
-        print(f'employee_object==========>{employee_object}')
-        return render(request, 'app/employee.html', {'employee_object': employee_object})
+        print(f"employee_object==========>{employee_object}")
+        return render(
+            request, "app/employee.html", {"employee_object": employee_object}
+        )
